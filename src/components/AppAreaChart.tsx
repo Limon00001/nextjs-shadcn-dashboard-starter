e9abc0cf0a2ab9bacbe2fa129df79713a795ec5d
Limon00001 <limonhossain82@gmail.com>
2025-05-01 00:47:11 +0600

    feat(chart): implement responsive area chart visualization

---
+++ src/components/AppAreaChart.tsx
@@ -0,0 +1,113 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 01 May, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// Client component
+'use client';
+
+// External imports
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+// Internal Imports
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from './ui/chart';
+
+// Chart Data
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+];
+
+// Chart Config
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: 'var(--chart-2)',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: 'var(--chart-1)',
+  },
+} satisfies ChartConfig;
+
+// Component
+const AppAreaChart = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-6">Total Visitors</h1>
+      <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+        <AreaChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <defs>
+            <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--color-desktop)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--color-desktop)"
+                stopOpacity={0.1}
+              />
+            </linearGradient>
+            <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--color-mobile)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--color-mobile)"
+                stopOpacity={0.1}
+              />
+            </linearGradient>
+          </defs>
+          <Area
+            dataKey="mobile"
+            type="natural"
+            fill="url(#fillMobile)"
+            fillOpacity={0.4}
+            stroke="var(--color-mobile)"
+            stackId="a"
+          />
+          <Area
+            dataKey="desktop"
+            type="natural"
+            fill="url(#fillDesktop)"
+            fillOpacity={0.4}
+            stroke="var(--color-desktop)"
+            stackId="a"
+          />
+        </AreaChart>
+      </ChartContainer>
+    </div>
+  );
+};
+
+// Export
+export default AppAreaChart;
